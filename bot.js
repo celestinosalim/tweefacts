2a01bfd400a24533e4ba62c0703bd7818c0990ec
celestinosalim <celestino.salim@gmail.com>
2019-08-11 18:42:00 -0400

    setup ready

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
-const { API_KEY } = process.env;
+const { API_KEY, SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET } = process.env;
 
 const Twit = require("twit");
 
 const T = new Twit({
-  consumer_key: "...",
-  consumer_secret: "...",
-  access_token: "...",
-  access_token_secret: "...",
+  consumer_key: API_KEY,
+  consumer_secret: SECRET_KEY,
+  access_token: ACCESS_TOKEN,
+  access_token_secret: ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   strictSSL: true // optional - requires SSL certificates to be valid.
 });
